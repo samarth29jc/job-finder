71020279e66c26e9af0df157925a4afad4a0c5a4
samarth29jc <samarth29jc@gmail.com>
2025-03-14 16:52:52 +0530

    merging

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://job-finder-k3n9.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
