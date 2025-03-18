4008d1bef5c60671dc3c7e8caae77ac8c3740069
samarth29jc <samarth29jc@gmail.com>
2025-03-18 07:32:10 +0530

    vercel config + broken link fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,4 +18,18 @@ export default defineConfig({
       },
     },
   },
+  // Ensure output file names include content hash for better caching
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+        },
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 }) 
